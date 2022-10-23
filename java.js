3352ea7ede07157ce533e2d3776ad907621ff3df
pro2442 <113509212+pro2442@users.noreply.github.com>
2022-10-23 10:02:48 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,57 @@
+// console.warn("№1");
+
+// let cars = [' adam', ' eva', ' kris']
+// while (cars[i]) {
+//   text += cars[i];
+//   i++;
+
+//   console.log(text);
+// }
+
+console.warn("№1");
+
+let r = 1;
+
+let massive = [2, 3, 4, 5];
+
+let i = 0;
+
+console.warn("через for");
+
+for (i = 0; i < massive.length; i++) {
+  r *= massive[i];
+  console.log(r);
+}
+
+console.warn("через while");
+
+let t = 1;
+
+let m = [2, 3, 4, 5];
+
+let u = 0;
+
+while (m[u]) {
+  t *= m[u];
+  u++;
+  console.log(t);
+}
+console.warn("№2");
+
+let a = [1, 2, 3, 4, 5];
+let b = 0;
+
+console.warn("через for");
+
+for (i = 0; i < a.length; i++) {
+  console.log(a[i]);
+}
+
+console.warn("через while");
+
+while (a[b]) {
+  b++;
+  console.log(b);
+}
+
+console.warn("№3");
